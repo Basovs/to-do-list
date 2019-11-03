33e36f2f3d4df29d7724d770c227fc9d24ad6a67
Janis Basovs <janisbasovs@Janiss-MacBook.lan>
2019-11-03 10:45:19 +0100

    Adding newest files

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log('janis')
+const appContainer = document.querySelector('#app-container')
+const addTaskInput = document.querySelector('#add-task-input')
+const btn = document.querySelector('#btn')
+const searchInput = document.querySelector('#search-input')
+const tasks = document.querySelector('#tasks')
+
+const task = document.createElement('div')
+task.classList.add('item-size')
+const p = document.createElement('p')
+task.appendChild(p)
+
+btn.addEventListener('click', function() {
+	p.textContent = `${addTaskInput.value}`
+	tasks.appendChild(task)
+})
