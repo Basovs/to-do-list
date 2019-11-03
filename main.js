f0d416d8bec37dab1a8a3930d586c62b440efac4
Janis Basovs <janisbasovs@Janiss-MacBook.lan>
2019-11-03 16:45:53 +0100

    Update 1

--- main.js
+++ main.js
@@ -4,12 +4,21 @@ const btn = document.querySelector('#btn')
 const searchInput = document.querySelector('#search-input')
 const tasks = document.querySelector('#tasks')
 
-const task = document.createElement('div')
-task.classList.add('item-size')
-const p = document.createElement('p')
-task.appendChild(p)
-
 btn.addEventListener('click', function() {
-	p.textContent = `${addTaskInput.value}`
-	tasks.appendChild(task)
+	if (addTaskInput.value !== '') {
+		const task = document.createElement('div')
+		task.classList.add('task-box')
+		const p = document.createElement('p')
+		p.textContent = `${addTaskInput.value}`
+		task.appendChild(p)
+		const deleteBtn = document.createElement('img')
+		deleteBtn.classList.add('delete-btn')
+		deleteBtn.src = 'img/delete.svg'
+		deleteBtn.addEventListener('click', function() {
+			task.classList.add('hide')
+		})
+		task.appendChild(deleteBtn)
+		tasks.appendChild(task)
+		addTaskInput.value = ''
+	}
 })
