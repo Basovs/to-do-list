95fe3dc9fea22b9e08deb317c816bf33503eccfe
Janis Basovs <janisbasovs@Janiss-MacBook.local>
2019-11-04 12:31:25 +0100

    Update 1.1

--- main.js
+++ main.js
@@ -4,21 +4,45 @@ const btn = document.querySelector('#btn')
 const searchInput = document.querySelector('#search-input')
 const tasks = document.querySelector('#tasks')
 
+function renderTask(taskValue) {
+	const task = document.createElement('div')
+	task.classList.add('task-box')
+	const p = document.createElement('p')
+	p.textContent = `${taskValue}`
+	task.appendChild(p)
+	const deleteBtn = document.createElement('img')
+	deleteBtn.classList.add('delete-btn')
+	deleteBtn.src = 'img/delete.svg'
+	deleteBtn.addEventListener('click', function() {
+		task.classList.add('hide')
+	})
+	task.appendChild(deleteBtn)
+	tasks.appendChild(task)
+}
+
+//Adds new task when btn is clicked
 btn.addEventListener('click', function() {
 	if (addTaskInput.value !== '') {
-		const task = document.createElement('div')
-		task.classList.add('task-box')
-		const p = document.createElement('p')
-		p.textContent = `${addTaskInput.value}`
-		task.appendChild(p)
-		const deleteBtn = document.createElement('img')
-		deleteBtn.classList.add('delete-btn')
-		deleteBtn.src = 'img/delete.svg'
-		deleteBtn.addEventListener('click', function() {
-			task.classList.add('hide')
-		})
-		task.appendChild(deleteBtn)
-		tasks.appendChild(task)
+		renderTask(addTaskInput.value)
 		addTaskInput.value = ''
 	}
+})
+
+//Draws a new task list
+function renderTaskList(arr) {
+	arr.forEach(element => {
+		element.parentElement.classList.remove('hide')
+	})
+}
+
+//On input in search field refresh the task list
+searchInput.addEventListener('input', function() {
+	const searchFor = event.currentTarget.value.toLowerCase()
+	const allTasks = Array.from(document.querySelectorAll('p'))
+	const filteredTasks = allTasks.filter(function(element) {
+		element.parentElement.classList.add('hide')
+		return element.textContent.toLowerCase().includes(searchFor)
+	})
+
+	renderTaskList(filteredTasks)
 })
